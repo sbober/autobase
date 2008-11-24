dcd517b86ca2e15e3d4e93bbff720dce3c04c8ff
Robert Fischer <robert.fischer@smokejumperit.com>
2008-11-23 21:03:30 -0500

    Wiped out the docs folder from storage, and completed the CreateGroovyScript script.

--- scripts/CreateMigration.groovy
+++ scripts/CreateMigration.groovy
@@ -7,33 +7,8 @@ Ant.property(environment:"env")
 grailsHome = Ant.antProject.properties."env.GRAILS_HOME"
 
 includeTargets << new File ( "${grailsHome}/scripts/Init.groovy" )
+includeTargets << new File ( "${autobasePluginDir}/scripts/_MigrationBase.groovy" )
 
 target ('default': "Creates a new migration") {
-  def name = args?.toString()
-	if(!name) {
-		Ant.input(addProperty:"migration.name", message:"Migration name not specified (press enter for auto-generated name)")
-		name = Ant.antProject.properties."migration.name"
-	}
-  if(name == null || SU.isBlank(name)) {
-    String timestamp = new SimpleDateFormat("yyyyMMdd.HHmmss").format(new Date())
-    name = timestamp
-  }
-  name = name.split(/\s+/).collect { SU.capitalize(it) }.join("")
-
-  def author = Props.instance.defaultAuthor
-  author = author.split(/\s+/).collect { SU.capitalize(it) }.join("")
-  def dir = "${basedir}/migrations/${author}"
-		      
-	def fileName = "${dir}/${name}Migration.groovy"
-		
-	Ant.sequential {  
-    mkdir(dir:dir)
-		copy(file:"${autobasePluginDir}/src/templates/artifacts/Migration.groovy",
-			 tofile:fileName) 
-		replace(file:fileName, 
-				token:"@AUTHOR@", value:author )
-    replace(file:fileName,
-        token:"@ID@", value:name )
-	}	                                                                            
-	println "Migration generated at ${fileName}"
+  depends(makeMigration)
 }
